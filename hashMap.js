8b3fec11d2f02fbad10446ca12218925725f0da5
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-02-01 05:34:19 +0200

    Add hash(key) function that returns a hash code

--- hashMap.js
+++ hashMap.js
@@ -32,4 +32,18 @@ class HashMap {
   set capacity(capacity) {
     this.#capacity = capacity;
   }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = hashCode % this.#capacity;
+    }
+
+    return hashCode;
+  }
 }
+
+const test = new HashMap();
