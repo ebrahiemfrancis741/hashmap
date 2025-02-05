4bacd61ac35feb4fe2f7b5e0a6deeb7772b4422d
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-02-05 05:18:46 +0200

    Remove unecessary files

--- hashMap.js
+++ hashMap.js
@@ -1,5 +1,4 @@
 import { LinkedList } from "ef-linked-list";
-import { createLinkedList } from "./linkedList.js";
 
 class HashMap {
   #data;
@@ -215,22 +214,4 @@ class HashMap {
   }
 }
 
-const test = new HashMap();
-test.set("apple", "red");
-test.set("banana", "yellow");
-test.set("carrot", "orange");
-test.set("dog", "brown");
-test.set("elephant", "gray");
-test.set("frog", "green");
-test.set("grape", "purple");
-test.set("hat", "black");
-test.set("ice cream", "white");
-test.set("jacket", "blue");
-test.set("kite", "pink");
-test.set("lion", "golden");
-test.set("moon", "silver");
-test.remove("moon");
-test.remove("lion");
-console.log(test.entries());
-console.log(test.has("jacket"));
-console.log(test.length());
+export { HashMap };
